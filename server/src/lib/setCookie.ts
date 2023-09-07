33277cc6103393a3a49aedd6c525b676f879724a
SuhravHussen <suhravshan@gmail.com>
2023-09-07 12:47:41 +0600

    added secure:true in cookie

--- server/src/lib/setCookie.ts
+++ server/src/lib/setCookie.ts
@@ -2,7 +2,7 @@ const setCookie = (res: any, token: any) => {
   const expirationDate = new Date(Date.now() + 24 * 60 * 60 * 1000); // One day from now
   res.cookie("token", token, {
     httpOnly: true,
-    secure: false,
+    secure: true,
     maxAge: 24 * 60 * 60 * 1000,
     sameSite: "None",
   });
